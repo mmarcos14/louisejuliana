448bc413a411b3d1ea6caeaa09f0383b5d326ff7
Ambroise ZOUNMENOU <zounmenouambroise22@gmail.com>
2026-02-25 12:32:38 -0500

    Update root route to use UserController

--- api/routes/web.php
+++ api/routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
+
+
+Route::get('/',[UserController::class,'index2']);
 
 require __DIR__.'/auth.php';
